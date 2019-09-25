bb0422961b3f0797beda1369fe70b940dd6b3661
jeina7 <jane1231001@gmail.com>
2019-09-25 15:25:41 +0900

    [update] separate functions

--- 8-puzzle/utils.py
+++ 8-puzzle/utils.py
@@ -1,13 +1,16 @@
-def move(node, direction):
-    blank_index = node.status.index(0)
-    new_state = copy.deepcopy(node.status)
+import copy
+from puzzle import Puzzle
+
+def move(puzzle, direction):
+    blank_index = puzzle.status.index(0)
+    new_state = copy.deepcopy(puzzle.status)
 
     if direction == 'up':
         if blank_index not in [0, 1, 2]:
             move_num = new_state[blank_index-3]
             new_state[blank_index] = move_num
             new_state[blank_index-3] = 0
-            return Node(new_state, node.depth+1, 'up', 0, node)
+            return Puzzle(new_state, puzzle.depth+1, 'up', 0, puzzle)
         else:
             return None
 
@@ -16,7 +19,7 @@ def move(node, direction):
             move_num = new_state[blank_index+3]
             new_state[blank_index] = move_num
             new_state[blank_index+3] = 0
-            return Node(new_state, node.depth+1, 'down', 0, node)
+            return Puzzle(new_state, puzzle.depth+1, 'down', 0, puzzle)
         else:
             return None
 
@@ -25,7 +28,7 @@ def move(node, direction):
             move_num = new_state[blank_index+1]
             new_state[blank_index] = move_num
             new_state[blank_index+1] = 0
-            return Node(new_state, node.depth+1, 'right', 0, node)
+            return Puzzle(new_state, puzzle.depth+1, 'right', 0, puzzle)
         else:
             return None
 
@@ -34,36 +37,36 @@ def move(node, direction):
             move_num = new_state[blank_index-1]
             new_state[blank_index] = move_num
             new_state[blank_index-1] = 0
-            return Node(new_state, node.depth+1, 'left', 0, node)
+            return Puzzle(new_state, puzzle.depth+1, 'left', 0, puzzle)
         else:
             return None
 
 
-def expand(node):
+def expand(puzzle):
     expanded = []
-    expanded.append(move(node, 'up'))
-    expanded.append(move(node, 'left'))
-    expanded.append(move(node, 'right'))
-    expanded.append(move(node, 'down'))
+    expanded.append(move(puzzle, 'up'))
+    expanded.append(move(puzzle, 'left'))
+    expanded.append(move(puzzle, 'right'))
+    expanded.append(move(puzzle, 'down'))
     expanded = [i for i in expanded if i]
     return expanded
 
 
 GOAL_STATE = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
-def h1(node):
+def h1(puzzle):
     # number of tiles out of place
     h = 0
-    for status_num, goal_num in zip(node.status, GOAL_STATE):
+    for status_num, goal_num in zip(puzzle.status, GOAL_STATE):
         if status_num != goal_num:
             h += 1
     return h
 
 
-def h2(node):
+def h2(puzzle):
     # summation of manhattan distance to the place
     h = 0
-    for status_idx, status_num in enumerate(node.status):
+    for status_idx, status_num in enumerate(puzzle.status):
         goal_idx = GOAL_STATE.index(status_num)
         status_x, status_y = status_idx % 3, status_idx // 3
         goal_x, goal_y = goal_idx % 3, goal_idx // 3
@@ -71,10 +74,26 @@ def h2(node):
     return h
 
 
-def get_path(start_node, goal_node):
+def get_path(start_puzzle, goal_puzzle):
     path = []
-    current_node = goal_node
-    while current_node.status != start_node.status:
-        path.insert(0, current_node.direction)
-        current_node = current_node.parent
+    current_puzzle = goal_puzzle
+    while current_puzzle.status != start_puzzle.status:
+        path.insert(0, current_puzzle.direction)
+        current_puzzle = current_puzzle.parent
+    return path
+
+
+def show_path(start_puzzle, goal_puzzle):
+    path = []
+    current_puzzle = goal_puzzle
+    while current_puzzle.status != start_puzzle.status:
+        path.insert(0, current_puzzle)
+        current_puzzle = current_puzzle.parent
+    path.insert(0, start_puzzle)
+    print("\n0 START!")
+    for idx, i in enumerate(path):
+        if i.direction:
+            print("{}".format(idx), i.direction)
+        i.show_map()
+    print("GOAL!!")
     return path
