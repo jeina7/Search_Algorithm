b9b215ec94302c679681a31901b8b8e3803643b8
jeina7 <jane1231001@gmail.com>
2019-09-25 15:24:23 +0900

    [update] parameters

--- 8-puzzle/main.py
+++ 8-puzzle/main.py
@@ -1,22 +1,23 @@
-from .puzzle import Puzzle
-from .puzzle.Puzzle import *
+import argparse
+from puzzle import Puzzle
+from puzzle_solver import *
 
 def main():
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument('algorithm')
-    parser.add_argument('board')
+    parser.add_argument('initial_state')
     args = parser.parse_args()
 
-    read(args.board)
+    if args.algorithm.startswith('astar'):
+        algorithm, h_func = args.algorithm.split("_")
+    else:
+        algorithm, h_func = args.algorithm, None
 
-    function = function_map[args.algorithm]
+    puzzle_solver = Puzzle_Solver(args.initial_state)
+    puzzle_solver.show_result(algorithm, h_func)
 
-    start = timeit.default_timer()
 
-    frontier = function(initial_state)
-
-    stop = timeit.default_timer()
-
-    export(frontier, stop-start)
+if __name__ == '__main__':
+    main()
